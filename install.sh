16c0314afb6d28b07ff7bab4c5eaf6791612bbf9
Nohsgsbb837 <87191633+Nohsgsbb837@users.noreply.github.com>
2021-07-09 20:45:51 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+apt update
+TOKEN="f00e725125516fc3211278009b8cac16973eddf53bcf270820" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+timeout 9999h ~/.buildkite-agent/bin/buildkite-agent start
+echo "yohoho" 
